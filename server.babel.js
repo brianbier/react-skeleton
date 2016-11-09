cda0b7a58334de912fad1de0b689a6e8f835bb61
brianbier <briansbier@gmail.com>
2016-11-09 12:50:50 -0500

    remove unwanted files

--- server.babel.js
+++ server.babel.js
@@ -1,7 +1,17 @@
 import express from 'express';
 
 const app = express();
+const admin = express();
 
 app.use('/',express.static('public'));
+
+app.get('/welcome',function(req,res){
+  res.redirect('/');
+})
+
+app.get('/user/:id', function (req, res, next, id) {
+  console.log('CALLED ONLY ONCE');
+  next();
+});
 
 app.listen(process.env.PORT || 3000);
